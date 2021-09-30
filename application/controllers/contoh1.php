4695960b74ba408ead2d491eab327ef61a13c937
FahrurRozii <fahrurrozi1700@gmail.com>
2021-09-30 19:21:03 +0700

    project pertama

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+public function index()
+ {
+ echo "<h1> mybio </h1>";
+ echo "nama: Fahrur Rozi <br>
+ Nim  : 12201336 <br>
+ tempat lahir : Bandar lampung <br>
+ tanggal lahir : 17 Jn 1994";
+ }
+}
